8e2af03e2389f71d7250e0d599b6c0e33ab5f804
danielvanpalo <daniel@grevensgastro.se>
2022-11-23 18:23:09 +0100

    innan javascript toggleButton

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ window.addEventListener('DOMContentLoaded', main);
 /**
  * 
  */
-function main();
+function main() {
+}
+
+
+
+function toggleButton() {
+const toggleButton = document.getElementById('toggleButton');
+const navList = document.getElementById('naviList')
+
+toggleButton.addEventListener("click", () => {
+    navList.classList.toggle("active");
+}) 
+}
